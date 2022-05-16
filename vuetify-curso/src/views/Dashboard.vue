ba2809921d8071feab7e708815d3fd9af5234307
Adriana <adrianafsamaral@gmail.com>
2022-05-16 14:34:05 -0300

    Navbar, Dashboard, Projects, Team

---
+++ vuetify-curso/src/views/Dashboard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="dashboard">
+   <h1>Dashboard</h1>
+     <!--<p class="red white--text">Lorem ipsum dolor</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-thin.font-italic text-uppercase">This is a subheading</p>
+    <p class="caption">this is a caption</p>
+
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn flat color="pink">click me</v-btn>
+    
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+   <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon right large>email</v-icon>
+    </v-btn>
+
+    <v-btn depressed small dark color="purple">
+     <v-icon>favorite</v-icon>
+    </v-btn>
+
+    <v-btn class="hidden-md-and-down">click me</v-btn> 
+    <v-btn class="hidden-md-and-up">click me</v-btn> 
+    <v-btn class="hidden-sm-only">click me</v-btn>  -->
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Dashboard'  
+}
+</script>
